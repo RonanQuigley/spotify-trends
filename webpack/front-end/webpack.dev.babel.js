20c2ad1a325d8dd807429572d32694eb379db154
ronanquigley <info@ronanquigley.com>
2018-05-15 19:41:54 +0100

    vscode chrome debugger fix

--- webpack/front-end/webpack.dev.babel.js
+++ webpack/front-end/webpack.dev.babel.js
@@ -1,6 +1,18 @@
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 import common from './webpack.common.babel';
+
+/* for vscode-chrome-debugger to work correctly we need to 
+change the devtool for testing and development this is because 
+vscode-chrome require non-inline source maps whilst mocha-webpack 
+works best with cheap and inlined source maps
+*/
+
+const devtool =
+    process.env.NODE_ENV === 'test'
+        ? 'inline-cheap-module-source-map'
+        : 'source-map';
+
 const dev = {
     mode: 'development',
     entry: {
@@ -8,7 +20,7 @@ const dev = {
         index: ['whatwg-fetch', './src/client/pages/index/'],
         results: ['whatwg-fetch', './src/client/pages/results/']
     },
-    devtool: '#source-map',
+    devtool: devtool,
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.EnvironmentPlugin({
