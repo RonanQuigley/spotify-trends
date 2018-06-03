d2daff0ea7f079a6729c7fcf60d40a0506a8361c
ronanquigley <info@ronanquigley.com>
2018-06-03 13:58:35 +0100

    added pie folder aliases

---
+++ test/common/react/pie/index.test.js
@@ -0,0 +1,18 @@
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+chai.use(chaiEnzyme());
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow();
+    });
+    it('should be able to render', () => {
+        expect(wrapper.isEmptyRender()).to.be.false;
+    });
+});
--- webpack/webpack.common.babel.js
+++ webpack/webpack.common.babel.js
@@ -4,12 +4,14 @@ const common = path.join(__dirname, './../src/common');
 const src = path.join(__dirname, './../src/');
 const charts = path.join(__dirname, './../src/common/react/charts');
 const fixtures = path.join(__dirname, './../test/fixtures');
+const pie = path.join(__dirname, './../src/common/react/pie');
 
 export default {
     resolve: {
         alias: {
             common: common,
             charts: charts,
+            pie: pie,
             fixtures: fixtures,
             src: src
         }
