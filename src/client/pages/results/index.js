7c8a3c86de18fc9da0136f9d513a03ac6a12fabd
ronanquigley <info@ronanquigley.com>
2018-06-12 18:29:17 +0100

    moved token update into window.onload callback

--- src/client/pages/results/index.js
+++ src/client/pages/results/index.js
@@ -3,14 +3,6 @@ import App from 'common/react/apps/results';
 import React from 'react';
 import { updateAllTokens } from 'src/client/utilities/tokens';
 
-// when working with react & data fixtures, we don't need to update tokens
-if (process.env.NODE_ENV !== 'development') {
-    updateAllTokens();
-} else {
-    console.warn(`In dev mode for react; tokens won't be updated`);
-    console.warn(`In dev mode for react; window __initial_state__ not cleared`);
-}
-
 window.onload = () => {
     const props = getInitProps();
     const app = <App childProps={props} />;
@@ -19,6 +11,17 @@ window.onload = () => {
         clearInitPropsFromWindow();
     }
     hydrateApp(app);
+
+    // when working with react & data fixtures, we don't need to update tokens
+    if (process.env.NODE_ENV !== 'development') {
+        // update the tokens after hydration has taken effect
+        updateAllTokens();
+    } else {
+        console.warn(`In dev mode for react; tokens won't be updated`);
+        console.warn(
+            `In dev mode for react; window __initial_state__ not cleared`
+        );
+    }
 };
 
 if (module.hot) {
