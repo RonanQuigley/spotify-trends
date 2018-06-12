d50c60604d147eee152e806e03044028b53d9946
ronanquigley <info@ronanquigley.com>
2018-06-12 19:03:47 +0100

    index page hot reloads the theme

--- src/client/pages/index/index.js
+++ src/client/pages/index/index.js
@@ -13,3 +13,21 @@ window.onload = () => {
     const app = <App />;
     hydrateApp(app);
 };
+
+if (module.hot) {
+    /* 
+        When using CommonJS, you MUST update dependencies 
+        manually by using re-requiring the modules in the callback. 
+        https://webpack.js.org/api/hot-module-replacement/ 
+    */
+    // accept changes from the theme
+    module.hot.accept('common/react/common/theme/index', () => {
+        const { hydrateApp } = require('src/client/react');
+        // this is a export default, so use .default
+        const theme = require('common/react/common/theme/index').default;
+        const app = <App />;
+        hydrateApp(app, theme);
+    });
+    // accept changes to self
+    module.hot.accept();
+}
