7bec6c7ba0c881eaa2dde1761dcbc0aa769ddaca
ronanquigley <info@ronanquigley.com>
2018-06-15 15:10:11 +0100

    turned off box shadow

--- src/common/react/charts/components/chart/components/element/styles.js
+++ src/common/react/charts/components/chart/components/element/styles.js
@@ -4,7 +4,7 @@ export default {
         height: '9rem',
         margin: '1.5% auto',
         display: 'flex',
-        boxShadow: '2px 2.6px 3px 0px rgba(0,0,0,0.12)',
+        boxShadow: 'initial',
         justifyContent: 'space-between',
         '@media (max-width : 670px)': {
             width: '100%',
