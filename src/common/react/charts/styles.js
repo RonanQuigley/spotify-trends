87342933249f0d4e0f3a8399305b1f4a65d6f5bc
ronanquigley <info@ronanquigley.com>
2018-06-15 16:43:13 +0100

    overflow scrolling

--- src/common/react/charts/styles.js
+++ src/common/react/charts/styles.js
@@ -12,6 +12,7 @@ export default {
         height: '100%',
         boxSizing: 'content-box',
         overflowY: 'scroll',
-        paddingRight: '29px'
+        paddingRight: '29px',
+        WebkitOverflowScrolling: 'touch'
     }
 };
