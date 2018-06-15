47a3a58c9890b7318cdcf43f86ea44390f63fe2b
ronanquigley <info@ronanquigley.com>
2018-06-15 15:14:39 +0100

    html meta changes

--- src/common/react/charts/components/chart/components/element/styles.js
+++ src/common/react/charts/components/chart/components/element/styles.js
@@ -4,7 +4,7 @@ export default {
         height: '9rem',
         margin: '1.5% auto',
         display: 'flex',
-        boxShadow: 'initial',
+        boxShadow: '2px 2.6px 3px 0px rgba(0,0,0,0.12)',
         justifyContent: 'space-between',
         '@media (max-width : 670px)': {
             width: '100%',
--- src/server/router/views/results/middleware.js
+++ src/server/router/views/results/middleware.js
@@ -166,7 +166,7 @@ export function renderResults(req, res, next) {
     <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>${pageTitle}</title>
             <link rel="stylesheet" href=${font}>
