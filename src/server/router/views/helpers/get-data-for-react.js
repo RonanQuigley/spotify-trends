44ecabee20d7a6ab928910aaf4e5c323b4d2b766
ronanquigley <info@ronanquigley.com>
2018-06-16 10:46:27 +0100

    removed unused handlebars code

--- src/server/router/views/helpers/get-data-for-react.js
+++
@@ -1,9 +0,0 @@
-export default props => {
-    return JSON.stringify({
-        artists: props.artists,
-        tracks: props.tracks,
-        mode: props.mode,
-        key: props.key,
-        average: props.average
-    });
-};
